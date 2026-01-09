fda0834edc536c09cc580e5187f2f778724c1f5b
wibbydashboard-code <wibbydashboard@gmail.com>
2026-01-08 19:31:18 -0600

    Add files via upload
    commit_v4

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ const uploadPayment = multer({
 });
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') {
